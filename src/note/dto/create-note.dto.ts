9328312f028e1ab8084d0bdc53f269555f8cef9c
prontoTheLegend <anasprog8@gmail.com>
2025-05-17 11:52:40 +0300

    Fix title and content of note to accept spaces

--- src/note/dto/create-note.dto.ts
+++ src/note/dto/create-note.dto.ts
@@ -2,12 +2,11 @@ import { IsEmail, IsString, Length, Matches } from 'class-validator';
 
 export class CreateNoteDto {
   @Length(0, 50)
-  @Matches(/^[A-Za-z]+$/, { message: 'title must contain only letters' })
+  @Matches(/^[A-Za-z\s]+$/, { message: 'title must contain only letters' })
   @IsString()
   title: string;
 
-  @Length(0, 50)
-  @Matches(/^[A-Za-z]+$/, { message: 'content must contain only letters' })
+  @Matches(/^[A-Za-z\s]+$/, { message: 'content must contain only letters' })
   @IsString()
   content: string;
 }
